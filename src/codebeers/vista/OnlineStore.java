fc260f59b5eb0fbdaf5810f19579d5521cc181f3
Ricardo Sandoval Pérez <rsandovalp@uoc.edu>
2022-12-19 12:21:28 +0100

    Se quitan los losg

--- src/codebeers/vista/OnlineStore.java
+++ src/codebeers/vista/OnlineStore.java
@@ -1,7 +1,13 @@
 package codebeers.vista;
 
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 public class OnlineStore{
     public static void main(String[] args){
+        LogManager.getLogManager().reset();
+        Logger globalLogger = Logger.getLogger(java.util.logging.Logger.GLOBAL_LOGGER_NAME);
+        globalLogger.setLevel(java.util.logging.Level.OFF);
         GestionOS gestion = new GestionOS();
         gestion.inicio();
     }
